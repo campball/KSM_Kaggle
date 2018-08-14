fc6f3d7968b34930b6b249e3adadd9275add259d
campball <campball@iu.edu>
2018-08-13 20:32:20 -0400

    added visualization and updated code

--- campbell_alexander_KSM_Kaggle.R
+++ campbell_alexander_KSM_Kaggle.R
@@ -1,7 +1,9 @@
-library(rpart)
+#KSM Consulting Data Exercise
+#Alex Campbell
+#8/13/2018
+
 library(randomForest)
-library(xgboost)
-setwd("X:/KSM_Kaggle")
+#setwd("X:/KSM_Kaggle")
 home_data = read.csv("train.csv")
 
 #ID is worthless
@@ -25,17 +27,27 @@ home_data$Condition2 = NULL
 #remove FireplaceQu because lots of N/A
 home_data$FireplaceQu = NULL
 
-
+#generate random forest model
+#random forest used here because no guarantee of linearity
+#and a large number of features
 forest = randomForest(SalePrice ~ ., data=home_data, na.action=na.exclude)
 
 #thanks to https://stackoverflow.com/questions/14860078/plot-multiple-lines-data-series-each-with-unique-color-in-r
 #and https://stats.stackexchange.com/questions/140474/single-column-plot-in-r-versus-gnuplot
 # and https://stackoverflow.com/questions/4862178/remove-rows-with-all-or-some-nas-missing-values-in-data-frame
 
+#plot predicted vs. actual home sale prices
 home_data_complete = home_data[complete.cases(home_data), ]
+#random forest model predicts well despite removal of incomplete data features
+#model more often underpredicted sale price than overpredicted for outliers
+#for further research, would be interesting to see if other populated features would improve prediction
 plot(x=home_data_complete$SalePrice/1000, y=forest$predicted/1000,
      xlab = "Actual Sale Price ($1000s)", ylab = "Predicted Sale Price ($1000s)",
-     main = "Sale Prices of Ames, Iowa Homes")
-#lines(x = seq(nrow(home_data_complete)), y=home_data_complete$SalePrice, col = "purple")
-#lines(x = seq(nrow(home_data_complete)), y=forest$predicted, col = "green")
+     main = "Sale Prices of Ames, Iowa Homes (2006-2010)
+     n = 1094")
 lines(x=seq(800000), y=seq(800000), col = "purple")
+text(600, 200, "r = 0.94")
+text(560, 503, "Ideal Prediction", col="purple")
+
+#good correlation!
+cor(home_data_complete$SalePrice, forest$predicted/1000)
